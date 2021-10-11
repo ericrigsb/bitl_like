412bfb0d9ed29aba722c0531661168b0fc98529f
Eric Rigsby <eric@rigsb.net>
2021-10-11 10:03:28 -0400

    updated logic to read hashtags file and only like unliked tweets

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,5 @@
 import os
 import tweepy
-import time
 import logging
 from dotenv import load_dotenv
 
@@ -23,14 +22,21 @@ auth.set_access_token(key, secret)
 
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
+with open('hashtags') as f:
+    hashtags = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+hashtags = [x.strip() for x in hashtags]
+
 def favorite():
-  for tweet in tweepy.Cursor(api.search_tweets, q="#beerleaguehockey", lang="en", result_type="recent").items(18):
-    status = api.get_status(tweet.id, tweet_mode = 'extended')
-    if not status.retweeted:  # Check if Retweet
-      try:
-        api.create_favorite(tweet.id)
-      except Exception as e:
-        logger.error("Error on like", exc_info=True)
-while True:
-  favorite()
-  time.sleep(900)
+  for hashtag in hashtags:
+    for tweet in tweepy.Cursor(api.search_tweets, q=hashtag, lang="en", result_type="recent").items(18):
+      status = api.get_status(tweet.id, tweet_mode = 'extended')
+      if not status.favorited:  # Check if already favorited
+        try:
+          print('\nTweet by: @' + tweet.user.screen_name)
+          print('\nTweet content: ' + tweet.text)
+          api.create_favorite(tweet.id)
+        except Exception as e:
+          logger.error("Error on like", exc_info=True)
+
+favorite()
